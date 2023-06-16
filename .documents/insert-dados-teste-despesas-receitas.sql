d957dc72e71c86d8ded05af681100f17609efa5a
Ribeiro <42475620+alexfariakof@users.noreply.github.com>
2023-06-15 22:01:31 -0300

    Insert mock Teste

---
+++ .documents/insert-dados-teste-despesas-receitas.sql
@@ -0,0 +1,266 @@
+﻿-- Janeiro
+INSERT INTO Despesa (Data, Descricao, Valor, DataVencimento, UsuarioId, CategoriaId)
+VALUES
+    ('2023-01-01', 'Conta de Luz', 150.00, '2023-01-10', 2, 14),
+    ('2023-01-02', 'Compra de mantimentos', 50.00, '2023-01-15', 2, 15),
+    ('2023-01-03', 'Serviço de Limpeza', 80.00, '2023-01-20', 2, 16),
+    ('2023-01-04', 'Consulta Médica', 200.00, '2023-01-25', 2, 17),
+    ('2023-01-05', 'Imposto de Renda', 300.00, '2023-01-05', 2, 18),
+    ('2023-01-06', 'Passagem de Ônibus', 10.00, '2023-01-10', 2, 19),
+    ('2023-01-07', 'Cinema', 20.50, '2023-01-15', 2, 20),
+    ('2023-01-08', 'Outros gastos', 15.20, '2023-01-20', 2, 21),
+    ('2023-01-09', 'Salário', 1000.00, '2023-01-25', 2, 22),
+    ('2023-01-10', 'Prêmio', 500.00, '2023-01-05', 2, 23);
+
+INSERT INTO Receita (Data, Descricao, Valor, UsuarioId, CategoriaId)
+VALUES
+    ('2023-01-05', 'Salário', 2000.00, 2, 22),
+    ('2023-01-15', 'Prêmio', 500.00, 2, 23),
+    ('2023-01-20', 'Investimento', 1000.00, 2, 24),
+    ('2023-01-25', 'Benefício', 300.00, 2, 25),
+    ('2023-01-30', 'Outros ganhos', 100.00, 2, 26);
+
+-- Fevereiro
+INSERT INTO Despesa (Data, Descricao, Valor, DataVencimento, UsuarioId, CategoriaId)
+VALUES
+    ('2023-02-01', 'Aluguel', 800.00, '2023-02-10', 2, 14),
+    ('2023-02-02', 'Compra de roupas', 100.00, '2023-02-15', 2, 15),
+    ('2023-02-03', 'Serviço de manutenção', 150.00, '2023-02-20', 2, 16),
+    ('2023-02-04', 'Consulta Médica', 250.00, '2023-02-25', 2, 17),
+    ('2023-02-05', 'Imposto de Renda', 350.00, '2023-02-05', 2, 18),
+    ('2023-02-06', 'Passagem de Ônibus', 15.00, '2023-02-10', 2, 19),
+    ('2023-02-07', 'Cinema', 25.50, '2023-02-15', 2, 20),
+    ('2023-02-08', 'Outros gastos', 20.20, '2023-02-20', 2, 21),
+    ('2023-02-09', 'Salário', 1000.00, '2023-02-25', 2, 22),
+    ('2023-02-10', 'Prêmio', 500.00, '2023-02-05', 2, 23);
+
+INSERT INTO Receita (Data, Descricao, Valor, UsuarioId, CategoriaId)
+VALUES
+    ('2023-02-05', 'Salário', 2000.00, 2, 22),
+    ('2023-02-15', 'Prêmio', 500.00, 2, 23),
+    ('2023-02-20', 'Investimento', 1000.00, 2, 24),
+    ('2023-02-25', 'Benefício', 300.00, 2, 25),
+    ('2023-02-28', 'Outros ganhos', 100.00, 2, 26);
+
+-- Março
+INSERT INTO Despesa (Data, Descricao, Valor, DataVencimento, UsuarioId, CategoriaId)
+VALUES
+    ('2023-03-01', 'Conta de Água', 100.00, '2023-03-10', 2, 14),
+    ('2023-03-02', 'Compra de Eletrônicos', 500.00, '2023-03-15', 2, 15),
+    ('2023-03-03', 'Serviço de Jardinagem', 120.00, '2023-03-20', 2, 16),
+    ('2023-03-04', 'Consulta Médica', 150.00, '2023-03-25', 2, 17),
+    ('2023-03-05', 'Imposto de Renda', 400.00, '2023-03-05', 2, 18),
+    ('2023-03-06', 'Passagem de Trem', 25.00, '2023-03-10', 2, 19),
+    ('2023-03-07', 'Teatro', 30.00, '2023-03-15', 2, 20),
+    ('2023-03-08', 'Outros gastos', 18.20, '2023-03-20', 2, 21),
+    ('2023-03-09', 'Salário', 1000.00, '2023-03-25', 2, 22),
+    ('2023-03-10', 'Prêmio', 500.00, '2023-03-05', 2, 23);
+
+INSERT INTO Receita (Data, Descricao, Valor, UsuarioId, CategoriaId)
+VALUES
+    ('2023-03-05', 'Salário', 2000.00, 2, 22),
+    ('2023-03-15', 'Prêmio', 500.00, 2, 23),
+    ('2023-03-20', 'Investimento', 1000.00, 2, 24),
+    ('2023-03-25', 'Benefício', 300.00, 2, 25),
+    ('2023-03-31', 'Outros ganhos', 100.00, 2, 26);
+
+-- Abril
+INSERT INTO Despesa (Data, Descricao, Valor, DataVencimento, UsuarioId, CategoriaId)
+VALUES
+    ('2023-04-01', 'Conta de Telefone', 80.00, '2023-04-10', 2, 14),
+    ('2023-04-02', 'Compra de Móveis', 700.00, '2023-04-15', 2, 15),
+    ('2023-04-03', 'Serviço de Encanamento', 90.00, '2023-04-20', 2, 16),
+    ('2023-04-04', 'Consulta Médica', 180.00, '2023-04-25', 2, 17),
+    ('2023-04-05', 'Imposto de Renda', 200.00, '2023-04-05', 2, 18),
+    ('2023-04-06', 'Passagem de Metrô', 5.00, '2023-04-10', 2, 19),
+    ('2023-04-07', 'Shows', 50.00, '2023-04-15', 2, 20),
+    ('2023-04-08', 'Outros gastos', 22.50, '2023-04-20', 2, 21),
+    ('2023-04-09', 'Salário', 1000.00, '2023-04-25', 2, 22),
+    ('2023-04-10', 'Prêmio', 500.00, '2023-04-05', 2, 23);
+
+INSERT INTO Receita (Data, Descricao, Valor, UsuarioId, CategoriaId)
+VALUES
+    ('2023-04-05', 'Salário', 2000.00, 2, 22),
+    ('2023-04-15', 'Prêmio', 500.00, 2, 23),
+    ('2023-04-20', 'Investimento', 1000.00, 2, 24),
+    ('2023-04-25', 'Benefício', 300.00, 2, 25),
+    ('2023-04-30', 'Outros ganhos', 100.00, 2, 26);
+
+-- Maio
+INSERT INTO Despesa (Data, Descricao, Valor, DataVencimento, UsuarioId, CategoriaId)
+VALUES
+    ('2023-05-01', 'Conta de Internet', 120.00, '2023-05-10', 2, 14),
+    ('2023-05-02', 'Compra de Eletrodomésticos', 800.00, '2023-05-15', 2, 15),
+    ('2023-05-03', 'Serviço de Pintura', 200.00, '2023-05-20', 2, 16),
+    ('2023-05-04', 'Consulta Médica', 150.00, '2023-05-25', 2, 17),
+    ('2023-05-05', 'Imposto de Renda', 400.00, '2023-05-05', 2, 18),
+    ('2023-05-06', 'Passagem de Barco', 30.00, '2023-05-10', 2, 19),
+    ('2023-05-07', 'Exposição de Arte', 40.00, '2023-05-15', 2, 20),
+    ('2023-05-08', 'Outros gastos', 25.80, '2023-05-20', 2, 21),
+    ('2023-05-09', 'Salário', 1000.00, '2023-05-25', 2, 22),
+    ('2023-05-10', 'Prêmio', 500.00, '2023-05-05', 2, 23);
+
+INSERT INTO Receita (Data, Descricao, Valor, UsuarioId, CategoriaId)
+VALUES
+    ('2023-05-05', 'Salário', 2000.00, 2, 22),
+    ('2023-05-15', 'Prêmio', 500.00, 2, 23),
+    ('2023-05-20', 'Investimento', 1000.00, 2, 24),
+    ('2023-05-25', 'Benefício', 300.00, 2, 25),
+    ('2023-05-31', 'Outros ganhos', 100.00, 2, 26);
+
+-- Junho
+INSERT INTO Despesa (Data, Descricao, Valor, DataVencimento, UsuarioId, CategoriaId)
+VALUES
+    ('2023-06-01', 'Conta de Gás', 90.00, '2023-06-10', 2, 14),
+    ('2023-06-02', 'Compra de Livros', 60.00, '2023-06-15', 2, 15),
+    ('2023-06-03', 'Serviço de Manicure', 40.00, '2023-06-20', 2, 16),
+    ('2023-06-04', 'Consulta Médica', 120.00, '2023-06-25', 2, 17),
+    ('2023-06-05', 'Imposto de Renda', 300.00, '2023-06-05', 2, 18),
+    ('2023-06-06', 'Passagem de Avião', 200.00, '2023-06-10', 2, 19),
+    ('2023-06-07', 'Shows', 70.00, '2023-06-15', 2, 20),
+    ('2023-06-08', 'Outros gastos', 35.50, '2023-06-20', 2, 21),
+    ('2023-06-09', 'Salário', 1000.00, '2023-06-25', 2, 22),
+    ('2023-06-10', 'Prêmio', 500.00, '2023-06-05', 2, 23);
+
+INSERT INTO Receita (Data, Descricao, Valor, UsuarioId, CategoriaId)
+VALUES
+    ('2023-06-05', 'Salário', 2000.00, 2, 22),
+    ('2023-06-15', 'Prêmio', 500.00, 2, 23),
+    ('2023-06-20', 'Investimento', 1000.00, 2, 24),
+    ('2023-06-25', 'Benefício', 300.00, 2, 25),
+    ('2023-06-30', 'Outros ganhos', 100.00, 2, 26);
+
+-- Julho
+INSERT INTO Despesa (Data, Descricao, Valor, DataVencimento, UsuarioId, CategoriaId)
+VALUES
+    ('2023-07-01', 'Conta de Energia', 150.00, '2023-07-10', 2, 14),
+    ('2023-07-02', 'Compra de Roupas', 200.00, '2023-07-15', 2, 15),
+    ('2023-07-03', 'Serviço de Limpeza', 80.00, '2023-07-20', 2, 16),
+    ('2023-07-04', 'Consulta Médica', 100.00, '2023-07-25', 2, 17),
+    ('2023-07-05', 'Imposto de Renda', 250.00, '2023-07-05', 2, 18),
+    ('2023-07-06', 'Passagem de Ônibus', 10.00, '2023-07-10', 2, 19),
+    ('2023-07-07', 'Cinema', 25.50, '2023-07-15', 2, 20),
+    ('2023-07-08', 'Outros gastos', 20.20, '2023-07-20', 2, 21),
+    ('2023-07-09', 'Salário', 1000.00, '2023-07-25', 2, 22),
+    ('2023-07-10', 'Prêmio', 500.00, '2023-07-05', 2, 23);
+
+INSERT INTO Receita (Data, Descricao, Valor, UsuarioId, CategoriaId)
+VALUES
+    ('2023-07-05', 'Salário', 2000.00, 2, 22),
+    ('2023-07-15', 'Prêmio', 500.00, 2, 23),
+    ('2023-07-20', 'Investimento', 1000.00, 2, 24),
+    ('2023-07-25', 'Benefício', 300.00, 2, 25),
+    ('2023-07-31', 'Outros ganhos', 100.00, 2, 26);
+
+-- Agosto
+INSERT INTO Despesa (Data, Descricao, Valor, DataVencimento, UsuarioId, CategoriaId)
+VALUES
+    ('2023-08-01', 'Conta de Água', 100.00, '2023-08-10', 2, 14),
+    ('2023-08-02', 'Compra de Eletrônicos', 500.00, '2023-08-15', 2, 15),
+    ('2023-08-03', 'Serviço de Jardinagem', 120.00, '2023-08-20', 2, 16),
+    ('2023-08-04', 'Consulta Médica', 150.00, '2023-08-25', 2, 17),
+    ('2023-08-05', 'Imposto de Renda', 400.00, '2023-08-05', 2, 18),
+    ('2023-08-06', 'Passagem de Trem', 25.00, '2023-08-10', 2, 19),
+    ('2023-08-07', 'Teatro', 30.00, '2023-08-15', 2, 20),
+    ('2023-08-08', 'Outros gastos', 18.75, '2023-08-20', 2, 21),
+    ('2023-08-09', 'Salário', 1000.00, '2023-08-25', 2, 22),
+    ('2023-08-10', 'Prêmio', 500.00, '2023-08-05', 2, 23);
+
+INSERT INTO Receita (Data, Descricao, Valor, UsuarioId, CategoriaId)
+VALUES
+    ('2023-08-05', 'Salário', 2000.00, 2, 22),
+    ('2023-08-15', 'Prêmio', 500.00, 2, 23),
+    ('2023-08-20', 'Investimento', 1000.00, 2, 24),
+    ('2023-08-25', 'Benefício', 300.00, 2, 25),
+    ('2023-08-31', 'Outros ganhos', 100.00, 2, 26);
+
+-- Setembro
+INSERT INTO Despesa (Data, Descricao, Valor, DataVencimento, UsuarioId, CategoriaId)
+VALUES
+    ('2023-09-01', 'Conta de Telefone', 80.00, '2023-09-10', 2, 14),
+    ('2023-09-02', 'Compra de Móveis', 700.00, '2023-09-15', 2, 15),
+    ('2023-09-03', 'Serviço de Encanamento', 90.00, '2023-09-20', 2, 16),
+    ('2023-09-04', 'Consulta Médica', 180.00, '2023-09-25', 2, 17),
+    ('2023-09-05', 'Imposto de Renda', 200.00, '2023-09-05', 2, 18),
+    ('2023-09-06', 'Passagem de Metrô', 5.00, '2023-09-10', 2, 19),
+    ('2023-09-07', 'Shows', 50.00, '2023-09-15', 2, 20),
+    ('2023-09-08', 'Outros gastos', 22.50, '2023-09-20', 2, 21),
+    ('2023-09-09', 'Salário', 1000.00, '2023-09-25', 2, 22),
+    ('2023-09-10', 'Prêmio', 500.00, '2023-09-05', 2, 23);
+
+INSERT INTO Receita (Data, Descricao, Valor, UsuarioId, CategoriaId)
+VALUES
+    ('2023-09-05', 'Salário', 2000.00, 2, 22),
+    ('2023-09-15', 'Prêmio', 500.00, 2, 23),
+    ('2023-09-20', 'Investimento', 1000.00, 2, 24),
+    ('2023-09-25', 'Benefício', 300.00, 2, 25),
+    ('2023-09-30', 'Outros ganhos', 100.00, 2, 26);
+
+-- Outubro
+INSERT INTO Despesa (Data, Descricao, Valor, DataVencimento, UsuarioId, CategoriaId)
+VALUES
+    ('2023-10-01', 'Aluguel', 800.00, '2023-10-10', 2, 14),
+    ('2023-10-02', 'Compra de Eletrônicos', 500.00, '2023-10-15', 2, 15),
+    ('2023-10-03', 'Serviço de Manutenção', 120.00, '2023-10-20', 2, 16),
+    ('2023-10-04', 'Consulta Médica', 150.00, '2023-10-25', 2, 17),
+    ('2023-10-05', 'Imposto de Renda', 400.00, '2023-10-05', 2, 18),
+    ('2023-10-06', 'Passagem de Ônibus', 20.00, '2023-10-10', 2, 19),
+    ('2023-10-07', 'Cinema', 25.50, '2023-10-15', 2, 20),
+    ('2023-10-08', 'Outros gastos', 18.75, '2023-10-20', 2, 21),
+    ('2023-10-09', 'Salário', 1000.00, '2023-10-25', 2, 22),
+    ('2023-10-10', 'Prêmio', 500.00, '2023-10-05', 2, 23);
+
+INSERT INTO Receita (Data, Descricao, Valor, UsuarioId, CategoriaId)
+VALUES
+    ('2023-10-05', 'Salário', 2000.00, 2, 22),
+    ('2023-10-15', 'Prêmio', 500.00, 2, 23),
+    ('2023-10-20', 'Investimento', 1000.00, 2, 24),
+    ('2023-10-25', 'Benefício', 300.00, 2, 25),
+    ('2023-10-31', 'Outros ganhos', 100.00, 2, 26);
+
+-- Novembro
+INSERT INTO Despesa (Data, Descricao, Valor, DataVencimento, UsuarioId, CategoriaId)
+VALUES
+    ('2023-11-01', 'Conta de Água', 100.00, '2023-11-10', 2, 14),
+    ('2023-11-02', 'Compra de Vestuário', 300.00, '2023-11-15', 2, 15),
+    ('2023-11-03', 'Serviço de Limpeza', 80.00, '2023-11-20', 2, 16),
+    ('2023-11-04', 'Consulta Médica', 100.00, '2023-11-25', 2, 17),
+    ('2023-11-06', 'Passagem de Trem', 25.00, '2023-11-10', 2, 19),
+    ('2023-11-07', 'Cinema', 25.50, '2023-11-15', 2, 20),
+    ('2023-11-08', 'Outros gastos', 20.20, '2023-11-20', 2, 21),
+    ('2023-11-09', 'Salário', 1000.00, '2023-11-25', 2, 22),
+    ('2023-11-10', 'Prêmio', 500.00, '2023-11-30', 2, 23);
+
+INSERT INTO Receita (Data, Descricao, Valor, UsuarioId, CategoriaId)
+VALUES
+    ('2023-11-06', 'Salário', 2000.00, 2, 22),
+    ('2023-11-15', 'Prêmio', 500.00, 2, 23),
+    ('2023-11-20', 'Investimento', 1000.00, 2, 24),
+    ('2023-11-25', 'Benefício', 300.00, 2, 25),
+    ('2023-11-30', 'Outros ganhos', 100.00, 2, 26);
+
+-- Dezembro
+INSERT INTO Despesa (Data, Descricao, Valor, DataVencimento, UsuarioId, CategoriaId)
+VALUES
+    ('2023-12-01', 'Conta de Energia', 150.00, '2023-12-10', 2, 14),
+    ('2023-12-02', 'Compra de Presentes', 400.00, '2023-12-15', 2, 15),
+    ('2023-12-03', 'Serviço de Manutenção', 120.00, '2023-12-20', 2, 16),
+    ('2023-12-04', 'Consulta Médica', 150.00, '2023-12-25', 2, 17),
+    ('2023-12-05', 'Imposto de Renda', 400.00, '2023-12-05', 2, 18),
+    ('2023-12-06', 'Passagem de Ônibus', 15.00, '2023-12-10', 2, 19),
+    ('2023-12-07', 'Cinema', 25.50, '2023-12-15', 2, 20),
+    ('2023-12-08', 'Outros gastos', 15.75, '2023-12-20', 2, 21),
+    ('2023-12-09', 'Salário', 1000.00, '2023-12-25', 2, 22),
+    ('2023-12-10', 'Prêmio', 500.00, '2023-12-05', 2, 23);
+
+INSERT INTO Receita (Data, Descricao, Valor, UsuarioId, CategoriaId)
+VALUES
+    ('2023-12-05', 'Salário', 2000.00, 2, 22),
+    ('2023-12-15', 'Prêmio', 500.00, 2, 23),
+    ('2023-12-20', 'Investimento', 1000.00, 2, 24),
+    ('2023-12-25', 'Benefício', 300.00, 2, 25),
+    ('2023-12-31', 'Outros ganhos', 100.00, 2, 26);
+
+
+
+
